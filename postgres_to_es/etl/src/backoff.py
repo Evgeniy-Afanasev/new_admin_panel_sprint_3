5facccefe4f436b71031a9843607647cfe2ef890
Евгений Афанасьев <79110345356@ya.ru>
2025-10-05 17:21:49 +0300

    update after first review

--- postgres_to_es/etl/src/backoff.py
+++ postgres_to_es/etl/src/backoff.py
@@ -1,19 +1,17 @@
 from functools import wraps
 from time import sleep
-
 from src.log import log
-
 
 def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
     """
     Декоратор для повторных попыток выполнения функций с задержкой.
     """
 
-    def func_wrapper(func):
+    def func_wrapper(func, max_attempts=10):
         @wraps(func)
         def inner(*args, **kwargs):
             attempt = 0
-            while attempt < 10:
+            while attempt < max_attempts:
                 try:
                     return func(*args, **kwargs)
                 except Exception as e:
@@ -21,6 +19,7 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
                     attempt += 1
                     delay = min(start_sleep_time * (factor ** attempt), border_sleep_time)
                     sleep(delay)
+            raise Exception(f'Функция {func.__name__} не смогла завершиться успешно после {max_attempts} попыток.')
 
         return inner
 
